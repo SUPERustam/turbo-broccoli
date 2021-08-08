37fec663e441ca3444f64ed641adbc083f23d41f
SUPERustam <498rustam@gmail.com>
2021-08-08 20:19:34 +0300

    Add support of requirements.txt file

--- venv.sh
+++ venv.sh
@@ -1,4 +1,5 @@
 python -m venv venv
 source venv/bin/activate
-pip install autoimport autopep8 isort
 pip install --upgrade pip setuptools
+pip install autoimport autopep8 isort
+pip install -r requirements.txt
