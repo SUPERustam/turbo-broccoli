a08c9b46c2a102a56e2326b67901400c9b89e49d
SUPERustam <498rustam@gmail.com>
2021-08-08 20:23:33 +0300

    Format and fix commit text

--- sync.sh
+++ sync.sh
@@ -5,8 +5,8 @@ gstatus=`git status --porcelain`
 if [ ${#gstatus} -ne 0 ]
 then
 
-	git add --all
-    git commit -m "(v1.3) Automated sync by sync.sh: Mermaid, more tags, new logic of tags and less zero-links!"
-	git pull origin master --rebase
-	git push origin master
+    git add --all
+    git commit -m "Automated sync by sync.sh:"
+    git pull origin master --rebase
+    git push origin master
 fi
