587315224f48190374635b7f56cb0831bc0d4be3
SUPERustam <498rustam@gmail.com>
2021-08-09 15:47:36 +0300

    Small fix in script

--- everyday.sh
+++ everyday.sh
@@ -1,2 +1,2 @@
 sudo pacman -Syu && sudo snap refresh && flatpak update
-pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U 
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
