6d012bda4ae0f4b855fcef4581c82fd3b55cbdb7
SUPERustam <498rustam@gmail.com>
2021-08-10 17:58:34 +0300

    ➕ Add flake8 and mypy

--- venv.sh
+++ venv.sh
@@ -1,5 +1,5 @@
 python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip setuptools
-pip install autoimport autopep8 isort
+pip install autoimport autopep8 isort flake8 mypy
 pip install -r requirements.txt
