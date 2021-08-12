d2b1e923e5396d36d1ceeb87c5c84d6461432190
SUPERustam <498rustam@gmail.com>
2021-08-12 16:31:20 +0300

    ⚙  New way to setup enviroment for C++ projects

--- venv.sh
+++ venv.sh
@@ -2,4 +2,3 @@ python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip setuptools mypy pylint autoflake isort autopep8
 pip install -r requirements.txt
-sudo pacman -S uncrustify cppcheck
