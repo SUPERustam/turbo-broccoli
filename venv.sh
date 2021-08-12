0bc32e533749c9cd1f6f3239e2063a99fcf3c8ff
SUPERustam <498rustam@gmail.com>
2021-08-12 14:28:42 +0300

    🌠 New good linters and fixers

--- venv.sh
+++ venv.sh
@@ -1,5 +1,5 @@
 python -m venv venv
 source venv/bin/activate
-pip install --upgrade pip setuptools
-pip install autoimport autopep8 isort flake8 mypy
+pip install --upgrade pip setuptools mypy pylint autoflake isort autopep8
 pip install -r requirements.txt
+sudo pacman -S uncrustify cppcheck
