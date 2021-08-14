8f258901e4183f9f770e25c8204f05a12b72cf0f
SUPERustam <498rustam@gmail.com>
2021-08-14 11:52:18 +0300

    🦚 .env instead of venv

--- everyday.sh
+++ everyday.sh
@@ -1,2 +1,3 @@
 sudo pacman -Syu && sudo snap refresh && flatpak update
+source .env/bin/activate
 pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
--- venv.sh
+++ venv.sh
@@ -1,5 +1,5 @@
 git config --global http.postBuffer 1048576000
-python -m venv venv
-source venv/bin/activate
+python -m venv .env
+source .env/bin/activate
 pip install --upgrade pip setuptools mypy pylint autoflake isort autopep8
 pip install -r requirements.txt
