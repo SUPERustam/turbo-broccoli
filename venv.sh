118f9ad7c492f95bcac4ac968c26bd6891381805
SUPERustam <498rustam@gmail.com>
2021-10-29 17:36:30 +0300

    🐏 Replace mypy to pyright

--- venv.sh
+++ venv.sh
@@ -3,5 +3,5 @@ git config --global http.version HTTP/1.1
 chmod +x sync.sh venv.sh everyday.sh
 python -m venv .env
 source .env/bin/activate
-pip install --upgrade pip setuptools mypy pylint autoflake isort autopep8
+pip install --upgrade pip setuptools pyright pylint autoflake isort autopep8
 pip install -r requirements.txt
