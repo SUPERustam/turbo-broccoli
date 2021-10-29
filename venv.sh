ab2d6958c219f90145514e462ec6e8f49b8bb408
SUPERustam <498rustam@gmail.com>
2021-10-29 17:48:27 +0300

    🍉 Add npm download and edit structure of some lines

--- venv.sh
+++ venv.sh
@@ -3,5 +3,8 @@ git config --global http.version HTTP/1.1
 chmod +x sync.sh venv.sh everyday.sh
 python -m venv .env
 source .env/bin/activate
-pip install --upgrade pip setuptools pyright pylint autoflake isort autopep8
+pip install --upgrade pip pylint autoflake isort 
+# for my config in Neovim
+pip install --upgrade pip setuptools pyright autopep8 pynvim 
+sudo npm install -g neovim 
 pip install -r requirements.txt
