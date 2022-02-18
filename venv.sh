fc685841c03f815516f7b819b465f891232f521b
SUPERustam <498rustam@gmail.com>
2022-02-18 19:56:51 +0300

    🎴 Update

--- venv.sh
+++ venv.sh
@@ -5,6 +5,6 @@ python -m venv .env
 source .env/bin/activate
 pip install --upgrade pip pylint autoflake isort 
 # for my config in Neovim
-pip install --upgrade pip setuptools pyright autopep8 pynvim debugpy
+pip install --upgrade pip setuptools pyright autopep8 pynvim debugpy neovim
 sudo npm install -g neovim 
 pip install -r requirements.txt
