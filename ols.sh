44530692a0c36ef4844b78669d7be35f4b255745
SUPERustam <498rustam@gmail.com>
2022-03-26 14:27:44 +0300

    🏊 Script for Olympics competitions

---
+++ ols.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[[ ! -f "testlib.h" ]] && cp ~/turbo-broccoli/testlib.h testlib.h
+g++ my.cpp -o .cpp.out && ./.cpp.out < 01 > ans 
+g++ check.cpp -o test.out && ./test.out 01 01.a ans 
--- venv.sh
+++ venv.sh
@@ -1,7 +1,7 @@
 git config --global http.postBuffer 5000000000
 git config --global http.version HTTP/1.1
 
-chmod +x sync.sh venv.sh 
+chmod +x sync.sh venv.sh ols.sh
 
 python -m venv .env
 source .env/bin/activate
