429b21611913ae8a5d4ec487e06f10ff15ec7fdd
SUPERustam <498rustam@gmail.com>
2022-11-28 20:40:53 +0300

    🙂 From Manjaro PC

--- update_apps.sh
+++ update_apps.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-pswrd=""
 url=https://zoom.us/client/latest/
 file=zoom_x86_64.pkg.tar.xz
+pswd="qwepoity"
 
 cd ~/Documents
 wget -qN $url$file
 
-echo $pswrd | sudo -S pacman 
+echo $pswd | sudo -S commandjfksjfklsajfklsjf 
 sudo pacman -U $file << 'EOF'
 Y
 EOF
 pacman -Q zoom
 
-echo $pswrd | sudo -S pacman 
+echo $pswd | sudo -S commandjfksjfklsajfklsjf
 yes | sudo pacman -Syu 
-
-echo $pswrd | sudo -S pacman 
+echo $pswd | sudo -S commandjfksjfklsajfklsjf
 sudo snap refresh 
-
+echo $pswd | sudo -S commandjfksjfklsajfklsjf
 flatpak update -y
