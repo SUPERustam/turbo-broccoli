64f973b00b75f90c4a26240f4031d52bbaca4680
SUPERustam <498rustam@gmail.com>
2023-01-03 21:20:45 +0300

    🎄 Setup git using venv.sh: .gitignore, .gitconfig

--- venv.sh
+++ venv.sh
@@ -1,16 +1,19 @@
+# git
 git config --global http.postBuffer 5000000000
 git config --global http.version HTTP/1.1
+git config --global core.editor nvim
+cp ~/turbo-broccoli/.gitignore .gitignore
 
 chmod +x sync.sh venv.sh ols.sh
 
-python -m venv .env
+python3 -m venv .env
 source .env/bin/activate
 pip install --upgrade pip pylint autoflake isort 
 
 # for my config in Neovim
-pip install --upgrade pip setuptools pyright autopep8 pynvim debugpy neovim
+pip3 install --upgrade pip setuptools pyright autopep8 pynvim debugpy neovim
 sudo npm install -g neovim 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # for vimspector
-cp /home/superustam/turbo-broccoli/.vimspector.json .vimspector.json
+cp ~/turbo-broccoli/.vimspector.json .vimspector.json
