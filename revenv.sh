79d574621326d4109c2aebcbef2c9225116e7388
SUPERustam <498rustam@gmail.com>
2023-09-07 21:59:43 +0300

    🦘 Minnor update of venv.sh and new revenv.sh – re-build venv for project

--- revenv.sh
+++ revenv.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# git
 cp ~/turbo-broccoli/.gitignore .gitignore
-cp ~/turbo-broccoli/.gitconfig ~/.gitconfig
-
-chmod +x sync.sh venv.sh ols.sh
-
 python3 -m venv .env
 source .env/bin/activate
 pip3 install --upgrade pip pylint autoflake isort 
-
-# for my config in Neovim
 pip3 install --upgrade pip setuptools pyright autopep8 pynvim debugpy neovim
-sudo npm install -g neovim 
 pip3 install -r requirements.txt
-
-# for vimspector
 cp ~/turbo-broccoli/.vimspector.json .vimspector.json
